d2749cdf9bb3b8f1ccf77bd744cf2a6a1bb6fdb4
Samirimayilov <114508115+Samirimayilov@users.noreply.github.com>
2023-04-15 16:22:39 -0500

    Create Task18_write_your_name.sql

---
+++ Task18_write_your_name.sql
@@ -0,0 +1,5 @@
+SELECT '**** ' AS "S", '  *   ' AS "A", ' *   * ' AS "M", ' **** ' AS "I", ' **** ' AS "R" FROM dual UNION ALL
+SELECT '*    ' AS "S", ' * *  ' AS "A", ' ** ** ' AS "M", '   *  ' AS "I", ' *   *' AS "R" FROM dual UNION ALL
+SELECT '***  ' AS "S", '****  ' AS "A", ' * * * ' AS "M", '   *  ' AS "I", ' **** ' AS "R" FROM dual UNION ALL
+SELECT '   * ' AS "S", '*  *  ' AS "A", ' *   * ' AS "M", '   *  ' AS "I", ' *  * ' AS "R" FROM dual UNION ALL
+SELECT '**** ' AS "S", '*  *  ' AS "A", ' *   * ' AS "M", ' **** ' AS "I", ' *   *' AS "R" FROM dual;
